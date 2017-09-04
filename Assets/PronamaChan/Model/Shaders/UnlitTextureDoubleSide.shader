dabe54216947db214818c2ba8692d32a5b7164b8
Tory <vickasvetlova@gmail.com>
2017-09-04 18:34:37 +0300

    init

---
+++ Assets/PronamaChan/Model/Shaders/UnlitTextureDoubleSide.shader
@@ -0,0 +1,16 @@
+﻿Shader "PronamaChan/Unlit/Texture(DoubleSide)" {
+	Properties{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+	}
+
+		SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		LOD 100
+
+		Pass{
+		Lighting Off
+		Cull Off
+		SetTexture[_MainTex]{ combine texture }
+	}
+	}
+}
---
+++ Assets/PronamaChan/Scripts/SpringCollider.cs
@@ -0,0 +1,24 @@
+﻿//
+//SpringBone.cs for PronamaChan
+//
+//Original Script is here:
+//ricopin / SpringCollider.cs
+//Rocket Jump : http://rocketjump.skr.jp/unity3d/109/
+//https://twitter.com/ricopin416
+//
+using UnityEngine;
+
+namespace PronamaChan
+{
+    public class SpringCollider : MonoBehaviour
+    {
+        //半径
+        public float radius = 0.5f;
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+}
